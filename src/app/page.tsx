a2b78ec335df745d7f353f8b23bf73a1b82bc45e
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-12-31 17:57:42 +0545

    hero card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/modules/home-page/Hero";
+import HeroCard from "@/components/modules/home-page/HeroCard";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <HeroCard />
     </>
   );
 }
