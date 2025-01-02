22e84f2daf80b921b0a90b57ed482d9749c2ebe3
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-02 10:23:31 +0545

    hero card issue fix

--- src/components/modules/home-page/HeroCard.tsx
+++ src/components/modules/home-page/HeroCard.tsx
@@ -16,6 +16,7 @@ const HeroCard = () => {
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
+                className="max-tablet-sm:h-[16px] max-tablet-sm:w-[16px]"
               >
                 <path
                   d="M1.375 22.625L22.625 1.375M22.625 1.375L6.6875 1.375M22.625 1.375V17.3125"
@@ -26,11 +27,11 @@ const HeroCard = () => {
                 />
               </svg>
             </div>
-            <div className="flex flex-col items-center justify-center gap-5 p-3 flex-grow">
-              <div className="flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-6 p-3 flex-grow">
+              <div className="flex items-center justify-center max-desktop-lg:h-[60px] max-desktop-lg:w-[60px] max-desktop-lg:gap-4">
                 {card.icon}
               </div>
-              <p className="text-white font-semibold text-lg text-center">
+              <p className="text-white font-semibold text-lg text-center max-desktop-lg:text-base max-tablet-sm:text-sm">
                 {card.title}
               </p>
             </div>
