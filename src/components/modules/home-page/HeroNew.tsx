48d7020553020365cfb64ca0bb5243caece463c9
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-06 14:15:18 +0545

    hero css fixed

--- src/components/modules/home-page/HeroNew.tsx
+++ src/components/modules/home-page/HeroNew.tsx
@@ -7,8 +7,9 @@ const HeroNew = () => {
   return (
     <>
       <div className="relative w-full h-full">
-        <div className="grid container grid-cols-1 tablet-lg:grid-cols-2 tablet-lg:h-[100vh] gap-2">
-          <div className="h-full">
+        <div className="grid container tablet-lg:h-[100vh] tablet-sm:grid-cols-2 gap-2 max-tablet-sm:flex max-tablet-sm:flex-col">
+          {/* left section */}
+          <div className="h-full max-tablet-sm:w-full">
             <div className="h-full pt-32 max-desktop-lg:pt-10">
               <div className="flex flex-col items-start justify-center gap-8 mt-5">
                 <h1 className="max-w-[610px] leading-tight font-semibold text-6xl max-desktop-2xl:text-5xl max-desktop-2xl:leading-snug max-tablet-lg:text-4xl max-tablet-lg:max-w-[350px] max-mobile-lg:text-3xl text-white">
@@ -35,7 +36,8 @@ const HeroNew = () => {
               </div>
             </div>
           </div>
-          <div className="z-100 w-1/2 absolute right-0">
+          {/* right section */}
+          <div className="z-100 w-1/2 absolute right-0 max-tablet-sm:relative max-tablet-sm:w-full">
             <div className="w-full h-full relative overflow-hidden">
               <Image
                 src={`${
