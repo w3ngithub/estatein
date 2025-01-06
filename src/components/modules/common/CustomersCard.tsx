018a9f8c575db245b31791bab62eff0f6d8764d0
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-06 15:00:51 +0545

    hero css fixed

--- src/components/modules/common/CustomersCard.tsx
+++ src/components/modules/common/CustomersCard.tsx
@@ -1,18 +1,18 @@
 const CustomersCard = () => {
   return (
-    <div className="relative flex flex-row gap-3 z-50">
+    <div className="relative flex flex-row gap-3 z-50 max-mobile-md:flex-wrap max-mobile-md:flex-grow max-mobile-md:border max-mobile-md:border-red-500">
       {/* 1 */}
-      <div className="flex flex-col gap-5 border-2 border-grey-shade-15 bg-grey-shade-10 rounded-md p-2 px-4">
-        <h1 className="text-4xl font-bold max-desktop-2xl:text-3xl text-white pt-1">
+      <div className="flex flex-col gap-5 border-2 border-grey-shade-15 bg-grey-shade-10 rounded-md p-2 px-4 max-mobile-md:items-center max-mobile-md:justify-center">
+        <h1 className="text-4xl font-bold max-desktop-2xl:text-3xl max-mobile-md:text-2xl text-white pt-1">
           200+
         </h1>
-        <p className="text-grey-shade-60 text-lg max-desktop-2xl:text-base">
+        <p className="text-grey-shade-60 text-lg max-desktop-2xl:text-base max-mobile-md:text-sm">
           Happy Customers
         </p>
       </div>
       {/* 2 */}
-      <div className="flex flex-col gap-5 border-2 border-grey-shade-15 bg-grey-shade-10 rounded-md p-2 px-4">
-        <h1 className="text-4xl font-bold max-desktop-2xl:text-3xl text-white pt-1">
+      <div className="flex flex-col gap-5 border-2 border-grey-shade-15 bg-grey-shade-10 rounded-md p-2 px-4 max-mobile-md:items-center max-mobile-md:justify-center">
+        <h1 className="text-4xl font-bold max-desktop-2xl:text-3xl max-mobile-md:text-2xl text-white pt-1">
           10k+
         </h1>
         <p className="text-grey-shade-60 text-lg max-desktop-2xl:text-base">
@@ -20,8 +20,8 @@ const CustomersCard = () => {
         </p>
       </div>
       {/* 3 */}
-      <div className="flex flex-col gap-5 border-2 border-grey-shade-15 bg-grey-shade-10 rounded-md p-2 px-4">
-        <h1 className="text-4xl font-bold max-desktop-2xl:text-3xl text-white pt-1">
+      <div className="flex flex-col gap-5 border-2 border-grey-shade-15 bg-grey-shade-10 rounded-md p-2 px-4 max-mobile-md:flex-grow max-mobile-md:items-center max-mobile-md:justify-center">
+        <h1 className="text-4xl font-bold max-desktop-2xl:text-3xl max-mobile-md:text-2xl text-white pt-1">
           16+
         </h1>
         <p className="text-grey-shade-60 text-lg max-desktop-2xl:text-base">
