c35df34043986654529fecd508a8c049f448b5bd
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-07 14:42:26 +0545

    svg fixed

--- src/utilityComponents/homePage/header.ts
+++
@@ -1,7 +0,0 @@
-const menuItems = [
-  { label: "Home", link: "/" },
-  { label: "About Us", link: "/about" },
-  { label: "Properties", link: "/properties" },
-  { label: "Services", link: "/services" },
-  { label: "Contact Us", link: "/contact" },
-];
