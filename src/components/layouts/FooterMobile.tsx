457fb360a11a2cd779cd18720e7b320c5ce0763b
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-07 12:01:29 +0545

    footer mobile

--- src/components/layouts/FooterMobile.tsx
+++ src/components/layouts/FooterMobile.tsx
@@ -5,15 +5,143 @@ const FooterMobile = () => {
   return (
     <div className="container">
       <div className="py-10">
-        <div className="grid grid-cols-2 divide-x-2 border-b border-grey-shade-d62">
-          <div className="divide-y-2">
-            <div>Home</div>
-            <div>Properties</div>
-            <div>Contact us</div>
+        <div className="grid grid-cols-2 divide-x-2 gap-5">
+          <div className="space-y-5">
+            <ul className="flex flex-col gap-3 list-none ml-4 py-2 border-b border-red-500">
+              <Link href="/">
+                <li className="text-xl font-medium text-grey-shade-60 mb-1 max-desktop-lg:text-lg max-tablet-sm:text-base">
+                  Home
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Hero Section
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Features
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Properties
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Testimonials
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  FAQ's
+                </li>
+              </Link>
+            </ul>
+            <ul className="flex flex-col gap-3 list-none ml-4 py-2 border-b border-red-500">
+              <Link href="/">
+                <li className="text-xl font-medium text-grey-shade-60 mb-1 max-desktop-lg:text-lg max-tablet-sm:text-base">
+                  Properties
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Portfolio
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Categories
+                </li>
+              </Link>
+            </ul>
+            <ul className="flex flex-col gap-3 list-none ml-4 py-2 border-b border-red-500">
+              <Link href="/">
+                <li className="text-xl font-medium text-grey-shade-60 mb-1 max-desktop-lg:text-lg max-tablet-sm:text-base">
+                  Contact Us
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Contact Form
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Our Offices
+                </li>
+              </Link>
+            </ul>
           </div>
-          <div className="divide-y-2">
-            <div>About Us</div>
-            <div>Services</div>
+          <div className="space-y-5">
+            <ul className="flex flex-col gap-3 list-none ml-4 py-2 border-b border-red-500">
+              <Link href="/">
+                <li className="text-xl font-medium text-grey-shade-60 mb-1 max-desktop-lg:text-lg max-tablet-sm:text-base">
+                  About Us
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Our Story
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Our Works
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  How It Works
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Our Team
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Our Clients
+                </li>
+              </Link>
+            </ul>
+            <div className="">
+              {" "}
+              <ul className="flex flex-col gap-3 list-none ml-4 py-2 border-b border-red-500">
+                <Link href="/">
+                  <li className="text-xl font-medium text-grey-shade-60 mb-1 max-desktop-lg:text-lg max-tablet-sm:text-base">
+                    Services
+                  </li>
+                </Link>
+                <Link href="/">
+                  <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                    Valuation Mastery
+                  </li>
+                </Link>
+                <Link href="/">
+                  <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                    Stategic Marketing
+                  </li>
+                </Link>
+                <Link href="/">
+                  <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                    Negotiation Wizardry
+                  </li>
+                </Link>
+                <Link href="/">
+                  <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                    Closing Success
+                  </li>
+                </Link>
+                <Link href="/">
+                  <li className="font-medium text-lg dark:text-white max-desktop-lg:text-base max-tablet-sm:text-sm">
+                    Property Management
+                  </li>
+                </Link>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
