f9b2458603d2b0c45c268fa24c09cef1cac203b0
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-08 16:45:33 +0545

    discover property section

--- src/app/property/page.tsx
+++ src/app/property/page.tsx
@@ -1,8 +1,10 @@
 import ContactForm from "@/components/modules/property-page/ContactForm";
+import DiscoveredProperty from "@/components/modules/property-page/DiscoverPossibility";
 
 const page = () => {
   return (
     <div className="bg-white-95 dark:bg-grey-shade-8">
+      <DiscoveredProperty />
       <ContactForm />
     </div>
   );
---
+++ src/utilityComponents/propertyPage/discoverProperty.tsx
@@ -0,0 +1,134 @@
+export const carouselDataDiscoverProperty = [
+  {
+    id: 1,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+  {
+    id: 2,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Urban Oasis - Life in the Heart of the City",
+      },
+    ],
+  },
+  {
+    id: 3,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Countryside Charm - Escape to Nature's Embrace",
+      },
+    ],
+  },
+  {
+    id: 4,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+  {
+    id: 5,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+  {
+    id: 6,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+  {
+    id: 7,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+  {
+    id: 8,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+  {
+    id: 9,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+  {
+    id: 10,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+  {
+    id: 11,
+    imageSrc: "/images/featured-property-first.png",
+    title: "Seaside Serenity Villa",
+    description:
+      "A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...",
+    details: [
+      {
+        pillName: "Coastal Escapes - Where Waves Beckon",
+      },
+    ],
+  },
+];
