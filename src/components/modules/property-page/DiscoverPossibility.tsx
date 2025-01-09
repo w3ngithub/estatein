0fb496a5cd4f5f569a46f914091f7105cf682b4f
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-09 12:33:42 +0545

    search property section responsive

--- src/components/modules/property-page/DiscoverPossibility.tsx
+++ src/components/modules/property-page/DiscoverPossibility.tsx
@@ -45,16 +45,16 @@ const DiscoveredProperty = () => {
   ];
   return (
     <div>
-      <div className="search-select-container flex flex-col gap-0 mt-[-38px]">
+      <div className="search-select-container flex flex-col gap-0 max-mobile-md:gap-5 mt-[-38px]">
         {/* search property field */}
         <div className="container flex flex-col justify-center items-center">
-          <div className="w-[80%] max-mobile-md:w-full p-2 bg-grey-shade-10 rounded-lg">
+          <div className="w-[80%] max-mobile-md:w-full max-mobile-md:p-1 p-2 bg-grey-shade-10 rounded-lg">
             <SearchProperty />
           </div>
         </div>
         {/* select fields */}
         <div className="container">
-          <div className="grid mobile-md:grid-cols-3 tablet-lg:grid-cols-5 gap-3 p-2 bg-grey-shade-10 rounded-lg">
+          <div className="grid mobile-md:grid-cols-3 tablet-lg:grid-cols-5 gap-3 p-2 max-mobile-md:p-5 bg-grey-shade-10 rounded-lg">
             <div>
               <SelectFieldWithIcon
                 placeholder="Location"
