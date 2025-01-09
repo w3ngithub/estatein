5dc3c1813c2c1118c169b35851a0a38b7014de3e
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-09 17:28:33 +0545

    property desc second sec

--- src/components/modules/property-detail-page/PropertyDescription.tsx
+++ src/components/modules/property-detail-page/PropertyDescription.tsx
@@ -27,7 +27,7 @@ const PropertyDescription = () => {
 
   return (
     <div className="container">
-      <div className="grid grid-cols-2 py-10">
+      <div className="grid grid-cols-2 gap-4 py-10">
         {/* first section */}
         <div className="flex flex-col gap-10 border border-grey-shade-15 p-5 rounded-lg">
           <div className="flex flex-col gap-4">
@@ -89,12 +89,46 @@ const PropertyDescription = () => {
             <h1 className="font-semibold text-2xl max-desktop-lg:text-xl">
               Key Features and Amenities
             </h1>
+            {/* first feature */}
             <div className="border-l-[1.5px] border-purple-shade-60 flex flex-row gap-2 pl-5 py-3 dark:bg-gradient-to-r from-[#1D1D1D] via-black to-black">
               <div>
                 <ThunderIcon />
               </div>
               <p className="font-medium text-lg text-grey-shade-60 max-desktop-lg:text-base">
                 Expansive oceanfront terrace for outdoor entertaining
+              </p>
+            </div>
+            <div className="border-l-[1.5px] border-purple-shade-60 flex flex-row gap-2 pl-5 py-3 dark:bg-gradient-to-r from-[#1D1D1D] via-black to-black">
+              <div>
+                <ThunderIcon />
+              </div>
+              <p className="font-medium text-lg text-grey-shade-60 max-desktop-lg:text-base">
+                Gourmet kitchen with top-of-the-line appliances
+              </p>
+            </div>
+            <div className="border-l-[1.5px] border-purple-shade-60 flex flex-row gap-2 pl-5 py-3 dark:bg-gradient-to-r from-[#1D1D1D] via-black to-black">
+              <div>
+                <ThunderIcon />
+              </div>
+              <p className="font-medium text-lg text-grey-shade-60 max-desktop-lg:text-base">
+                Private beach access for morning strolls and sunset views
+              </p>
+            </div>
+            <div className="border-l-[1.5px] border-purple-shade-60 flex flex-row gap-2 pl-5 py-3 dark:bg-gradient-to-r from-[#1D1D1D] via-black to-black">
+              <div>
+                <ThunderIcon />
+              </div>
+              <p className="font-medium text-lg text-grey-shade-60 max-desktop-lg:text-base">
+                Master suite with a spa-inspired bathroom and ocean-facing
+                balcony
+              </p>
+            </div>
+            <div className="border-l-[1.5px] border-purple-shade-60 flex flex-row gap-2 pl-5 py-3 dark:bg-gradient-to-r from-[#1D1D1D] via-black to-black">
+              <div>
+                <ThunderIcon />
+              </div>
+              <p className="font-medium text-lg text-grey-shade-60 max-desktop-lg:text-base">
+                Private garage and ample storage space
               </p>
             </div>
           </div>
