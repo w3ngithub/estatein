8f97c80f0d6ee6bf6034c8b2633f07dd780a549c
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-09 16:46:09 +0545

    property desc first sec

--- src/components/modules/property-detail-page/PropertyDescription.tsx
+++ src/components/modules/property-detail-page/PropertyDescription.tsx
@@ -4,12 +4,14 @@ import React from "react";
 const PropertyDescription = () => {
   return (
     <div className="container">
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 py-10">
         {/* first section */}
-        <div className="flex flex-col gap-3 border border-grey-shade-15 p-5 rounded-lg">
-          <div className="flex flex-col gap-2">
-            <h2 className="font-semibold text-2xl text-white">Description</h2>
-            <p className="font-medium text-lg text-grey-60">
+        <div className="flex flex-col gap-10 border border-grey-shade-15 p-5 rounded-lg">
+          <div className="flex flex-col gap-4">
+            <h2 className="font-semibold text-2xl text-white max-desktop-lg:text-xl">
+              Description
+            </h2>
+            <p className="font-medium text-lg text-grey-60 max-desktop-lg:text-base">
               Discover your own piece of paradise with the Seaside Serenity
               Villa. T With an open floor plan, breathtaking ocean views from
               every room, and direct access to a pristine sandy beach, this
@@ -18,31 +20,43 @@ const PropertyDescription = () => {
           </div>
           <div className="grid grid-cols-3">
             <div className="flex flex-col gap-3">
-              <div className="flex flex-row gap-1">
+              <div className="flex flex-row items-center gap-1">
                 <div>
                   <BedIcon />
                 </div>
-                <p className="font-medium text-lg text-grey-60">Bedrooms</p>
+                <p className="font-medium text-lg text-grey-shade-60 max-desktop-lg:text-sm">
+                  Bedrooms
+                </p>
               </div>
-              <h1 className="font-semibold text-2xl">04</h1>
+              <h1 className="font-semibold text-2xl max-desktop-lg:text-xl">
+                04
+              </h1>
             </div>
             <div className="flex flex-col gap-3">
-              <div className="flex flex-row gap-1">
+              <div className="flex flex-row items-center gap-1">
                 <div>
                   <ShowerIcon />
                 </div>
-                <p className="font-medium text-lg text-grey-60">Bathrooms</p>
+                <p className="font-medium text-lg text-grey-shade-60 max-desktop-lg:text-sm">
+                  Bathrooms
+                </p>
               </div>
-              <h1 className="font-semibold text-2xl">03</h1>
+              <h1 className="font-semibold text-2xl max-desktop-lg:text-xl">
+                03
+              </h1>
             </div>
             <div className="flex flex-col gap-3">
-              <div className="flex flex-row gap-1">
+              <div className="flex flex-row items-center gap-1">
                 <div>
                   <AreaIcon />
                 </div>
-                <p className="font-medium text-lg text-grey-60">Area</p>
+                <p className="font-medium text-lg text-grey-shade-60 max-desktop-lg:text-sm">
+                  Area
+                </p>
               </div>
-              <h1 className="font-semibold text-2xl">2,500 Square Feet</h1>
+              <h1 className="font-semibold text-2xl max-desktop-lg:text-xl">
+                2,500 Square Feet
+              </h1>
             </div>
           </div>
         </div>
