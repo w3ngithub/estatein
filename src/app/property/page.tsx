149761ec52f99186d67c15784d40a1266aa2bdaf
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-09 09:16:59 +0545

    dream property section

--- src/app/property/page.tsx
+++ src/app/property/page.tsx
@@ -1,9 +1,11 @@
 import ContactForm from "@/components/modules/property-page/ContactForm";
 import DiscoveredProperty from "@/components/modules/property-page/DiscoverPossibility";
+import DreamProperty from "@/components/modules/property-page/DreamProperty";
 
 const page = () => {
   return (
     <div className="bg-white-95 dark:bg-grey-shade-8">
+      <DreamProperty />
       <DiscoveredProperty />
       <ContactForm />
     </div>
---
+++ src/components/modules/property-page/DreamProperty.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const DreamProperty = () => {
+  return (
+    <div className="bg-gradient-to-r from-[#1D1D1D] via-black to-black">
+      <div className="container flex items-center h-[350px]">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-5xl font-semibold max-desktop-lg:text-4xl max-tablet-sm:text-[28px] dark:text-white">
+            Find Your Dream Property
+          </h1>
+          <p className="max-w-[900px] dark:text-grey-shade-60 text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
+            Welcome to Estatein, where your dream property awaits in every
+            corner of our beautiful world. Explore our curated selection of
+            properties, each offering a unique story and a chance to redefine
+            your life. With categories to suit every dreamer, your journey
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DreamProperty;
