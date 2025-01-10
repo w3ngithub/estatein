f9d31bf5f293c6b0b1785c8c7758b9d157720d95
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-10 11:49:03 +0545

    pricing details third section

--- src/components/modules/property-detail-page/PricingDetails.tsx
+++ src/components/modules/property-detail-page/PricingDetails.tsx
@@ -49,7 +49,8 @@ const PricingDetails = () => {
             <div className="flex flex-col gap-3">
               {/* 1st card */}
               <div className="p-5 border border-grey-shade-15 rounded-lg">
-                <div className="flex flex-row items-center justify-between gap-2">
+                {/* 1st card: first row */}
+                <div className="flex flex-row items-center justify-between gap-2 py-5">
                   <h2 className="text-2xl font-semibold max-desktop-lg:text-xl max-tablet-sm:text-lg">
                     Additional Fees
                   </h2>
@@ -57,7 +58,24 @@ const PricingDetails = () => {
                     Learn More
                   </Button>
                 </div>
-                <div></div>
+                <div className="flex flex-row gap-3 border-t border-grey-shade-15 pt-5">
+                  <div className="flex flex-col gap-3">
+                    <p className="dark:text-grey-shade-60 font-medium text-lg leading-6 max-desktop-lg:text-sm">
+                      Property Transfer Tax
+                    </p>
+                    <div className="flex flex-row gap-3">
+                      <h1 className="font-semibold text-2xl max-desktop-lg:text-xl max-tablet-sm:text-lg">
+                        $25,000
+                      </h1>
+                      <div className="flex flex-row justify-center items-center gap-1 border border-grey-shade-15 bg-grey-shade-10 px-3 py-2 rounded-full">
+                        <p className="text-grey-shade-60 text-lg max-desktop-lg:text-sm">
+                          Based on the sale price and local regulations
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>legal fees</div>
+                </div>
                 <div></div>
                 <div></div>
               </div>
