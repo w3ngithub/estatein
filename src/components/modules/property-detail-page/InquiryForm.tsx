2b2b0e0e32fd51a61fc63f25b34018dc303d4b91
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-10 14:50:10 +0545

    inquiry form second section

--- src/components/modules/property-detail-page/InquiryForm.tsx
+++ src/components/modules/property-detail-page/InquiryForm.tsx
@@ -1,3 +1,5 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { ThreeStars } from "@/svgs/HomePageSvg";
 import React from "react";
 
@@ -5,8 +7,8 @@ const InquiryForm = () => {
   return (
     <>
       <div className="container">
-        <div className="grid grid-cols-12">
-          <div className="col-span-4">
+        <div className="grid grid-cols-12 border border-red-500">
+          <div className="col-span-4 border border-red-500">
             <div className="flex flex-col gap-3">
               <div>
                 <ThreeStars />
@@ -22,7 +24,73 @@ const InquiryForm = () => {
               </p>
             </div>
           </div>
-          <div className="col-span-8">form</div>
+          <div className="col-span-8 border border-red-500">
+            <div className="flex flex-col gap-5 border border-grey-shade-15 rounded-lg p-5">
+              <div className="grid grid-cols-2 gap-3">
+                <div className="flex flex-col gap-3">
+                  <Label
+                    htmlFor="firstName"
+                    className="font-semibold text-xl max-desktop-lg:text-base"
+                  >
+                    First Name
+                  </Label>
+                  <Input
+                    id="firstName"
+                    type="text"
+                    placeholder="Enter First Name"
+                    className="h-16 max-desktop-lg:h-14 border border-grey-shade-15 dark:bg-grey-shade-10"
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Label
+                    htmlFor="lastName"
+                    className="font-semibold text-xl max-desktop-lg:text-base"
+                  >
+                    Last Name
+                  </Label>
+                  <Input
+                    id="lastName"
+                    type="text"
+                    placeholder="Enter Last Name"
+                    className="h-16 max-desktop-lg:h-14 border border-grey-shade-15 dark:bg-grey-shade-10"
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-3">
+                <div className="flex flex-col gap-3">
+                  <Label
+                    htmlFor="email"
+                    className="font-semibold text-xl max-desktop-lg:text-base"
+                  >
+                    Email
+                  </Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="Enter your Email"
+                    className="h-16 max-desktop-lg:h-14 border border-grey-shade-15 dark:bg-grey-shade-10"
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Label
+                    htmlFor="phoneNumber"
+                    className="font-semibold text-xl max-desktop-lg:text-base"
+                  >
+                    Last Name
+                  </Label>
+                  <Input
+                    id="phoneNumber"
+                    type="number"
+                    placeholder="Enter phone Number"
+                    className="h-16 max-desktop-lg:h-14 border border-grey-shade-15 dark:bg-grey-shade-10"
+                  />
+                </div>
+              </div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
         </div>
       </div>
     </>
