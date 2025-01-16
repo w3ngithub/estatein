71a153cefe6d0dac1d44648a217467c86e18a2ff
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-16 13:39:14 +0545

    achievement section responsive

--- src/components/modules/about-us/Achievements.tsx
+++ src/components/modules/about-us/Achievements.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Achievements = () => {
   return (
-    <section className="container">
+    <section className="container py-10">
       <div className="flex flex-col gap-10">
         <div className="flex flex-col gap-3">
           {/* 3 star logo */}
@@ -20,7 +20,7 @@ const Achievements = () => {
           </p>
         </div>
         {/* card */}
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 space-x-4">
           <div className="p-2 bg-grey-shade-d61 rounded-lg">
             <div className="bg-grey-shade-8 rounded-lg">
               <div className="flex flex-col gap-5 p-8">
@@ -31,6 +31,33 @@ const Achievements = () => {
                   With over 3 years in the industry, we've amassed a wealth of
                   knowledge and experience, becoming a go-to resource for all
                   things real estate.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="p-2 bg-grey-shade-d61 rounded-lg">
+            <div className="bg-grey-shade-8 rounded-lg">
+              <div className="flex flex-col gap-5 p-8">
+                <h2 className="font-semibold text-3xl dark:text-white max-desktop-lg:text-xl max-tablet-sm:text-lg">
+                  Happy Clients
+                </h2>
+                <p className="dark:text-grey-shade-60 font-medium text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  Our greatest achievement is the satisfaction of our clients.
+                  Their success stories fuel our passion for what we do.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="p-2 bg-grey-shade-d61 rounded-lg">
+            <div className="bg-grey-shade-8 rounded-lg">
+              <div className="flex flex-col gap-5 p-8">
+                <h2 className="font-semibold text-3xl dark:text-white max-desktop-lg:text-xl max-tablet-sm:text-lg">
+                  Industry Recognition
+                </h2>
+                <p className="dark:text-grey-shade-60 font-medium text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
+                  We've earned the respect of our peers and industry leaders,
+                  with accolades and awards that reflect our commitment to
+                  excellence.
                 </p>
               </div>
             </div>
