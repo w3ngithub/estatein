10e3dbf70f88eb4af8c993df34e669a72ccaf541
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-16 16:08:33 +0545

    navigate experience section

--- src/components/modules/about-us/NavigateExperience.tsx
+++ src/components/modules/about-us/NavigateExperience.tsx
@@ -20,7 +20,31 @@ const NavigateExperience = () => {
           </p>
         </div>
         {/* card */}
-        <div className="grid grid-cols-3"></div>
+        <div className="grid grid-cols-3">
+          <div className="flex flex-col">
+            <p className="border-l border-purple-shade-60 pl-5 py-2 font-medium text-xl max-desktop-lg:">
+              Step 01
+            </p>
+            <div
+              className="p-5 flex flex-col gap-3 border border-grey-shade-15"
+              style={{
+                border: "1px solid #262626",
+                background:
+                  "linear-gradient(135deg, rgba(112, 59, 274, 1) 2%, rgba(0, 0, 0, 1) 17%)",
+              }}
+            >
+              <h2 className="font-semibold text-2xl max-desktop-lg:">
+                Discover a World of Possibilities
+              </h2>
+              <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:">
+                Your journey begins with exploring our carefully curated
+                property listings. Use our intuitive search tools to filter
+                properties based on your preferences, including location, type,
+                size, and budget.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
