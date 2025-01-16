7ab31aceac0660b2301f78172384486c1dd64fa4
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-16 13:56:25 +0545

    achievement section dynamic

--- src/components/modules/about-us/Achievements.tsx
+++ src/components/modules/about-us/Achievements.tsx
@@ -2,6 +2,27 @@ import { ThreeStars } from "@/svgs/HomePageSvg";
 import React from "react";
 
 const Achievements = () => {
+  const cardItems = [
+    {
+      id: 1,
+      title: "3+ Years of Excellence",
+      description:
+        "With over 3 years in the industry, we've amassed a wealth of knowledge and experience, becoming a go-to resource for all things real estate.",
+    },
+    {
+      id: 2,
+      title: "Happy Clients",
+      description:
+        "Our greatest achievement is the satisfaction of our clients. Their success stories fuel our passion for what we do.",
+    },
+    {
+      id: 3,
+      title: "Industry Recognition",
+      description:
+        "We've earned the respect of our peers and industry leaders, with accolades and awards that reflect our commitment to excellence.",
+    },
+  ];
+
   return (
     <section className="container py-10">
       <div className="flex flex-col gap-10">
@@ -21,47 +42,20 @@ const Achievements = () => {
         </div>
         {/* card */}
         <div className="grid mobile-xl:grid-cols-3 mobile-xl:space-x-4 max-mobile-xl:gap-5">
-          <div className="p-2 bg-grey-shade-d61 rounded-lg">
-            <div className="bg-grey-shade-8 rounded-lg h-full">
-              <div className="flex flex-col gap-5 p-8">
-                <h2 className="font-semibold text-3xl dark:text-white max-desktop-lg:text-xl max-tablet-sm:text-lg">
-                  3+ Years of Excellence
-                </h2>
-                <p className="dark:text-grey-shade-60 font-medium text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
-                  With over 3 years in the industry, we've amassed a wealth of
-                  knowledge and experience, becoming a go-to resource for all
-                  things real estate.
-                </p>
+          {cardItems.map((item) => (
+            <div key={item.id} className="p-2 bg-grey-shade-d61 rounded-lg">
+              <div className="bg-grey-shade-8 rounded-lg h-full">
+                <div className="flex flex-col gap-5 p-8">
+                  <h2 className="font-semibold text-3xl dark:text-white max-desktop-lg:text-xl max-tablet-sm:text-lg">
+                    {item.title}
+                  </h2>
+                  <p className="dark:text-grey-shade-60 font-medium text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
+                    {item.description}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="p-2 bg-grey-shade-d61 rounded-lg">
-            <div className="bg-grey-shade-8 rounded-lg h-full">
-              <div className="flex flex-col gap-5 p-8">
-                <h2 className="font-semibold text-3xl dark:text-white max-desktop-lg:text-xl max-tablet-sm:text-lg">
-                  Happy Clients
-                </h2>
-                <p className="dark:text-grey-shade-60 font-medium text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
-                  Our greatest achievement is the satisfaction of our clients.
-                  Their success stories fuel our passion for what we do.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="p-2 bg-grey-shade-d61 rounded-lg">
-            <div className="bg-grey-shade-8 rounded-lg h-full">
-              <div className="flex flex-col gap-5 p-8">
-                <h2 className="font-semibold text-3xl dark:text-white max-desktop-lg:text-xl max-tablet-sm:text-lg">
-                  Industry Recognition
-                </h2>
-                <p className="dark:text-grey-shade-60 font-medium text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
-                  We've earned the respect of our peers and industry leaders,
-                  with accolades and awards that reflect our commitment to
-                  excellence.
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
