6e999e0803effa5af200eb8c7eab2b97fc39174d
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-17 13:55:45 +0545

    our team section responsive

--- src/components/modules/about-us/MeetTeam.tsx
+++ src/components/modules/about-us/MeetTeam.tsx
@@ -9,6 +9,29 @@ import Image from "next/image";
 import React from "react";
 
 const MeetTeam = () => {
+  const teamMembers = [
+    {
+      name: "Max Mitchell",
+      role: "Founder",
+      imageSrc: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/about-us-team-one.png`,
+    },
+    {
+      name: "Sarah Johnson",
+      role: "Chief Real Estate Officer",
+      imageSrc: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/about-us-team-one.png`,
+    },
+    {
+      name: "David Brown",
+      role: "Head of Property Management",
+      imageSrc: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/about-us-team-one.png`,
+    },
+    {
+      name: "Michael Turner",
+      role: "Legal Counsel",
+      imageSrc: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/about-us-team-one.png`,
+    },
+  ];
+
   return (
     <section className="container py-10">
       <div className="flex flex-col gap-16">
@@ -28,159 +51,46 @@ const MeetTeam = () => {
         </div>
         {/* card */}
         <div className="grid mobile-lg:grid-cols-2 desktop-md:grid-cols-4 gap-5">
-          {/* 1 */}
-          <div className="flex flex-col items-center justify-center p-5 border border-grey-shade-15 rounded-lg">
-            <div>
-              <Image
-                src={`${
-                  process.env.NEXT_PUBLIC_BASE_PATH +
-                  "/images/about-us-team-one.png"
-                }`}
-                width={317}
-                height={253}
-                alt="profile icon"
-              />
-            </div>
-            <div className="flex flex-col gap-5 justify-center items-center mt-[-25px]">
-              <div className="border-none bg-purple-shade-60 py-3 px-5 rounded-full cursor-pointer">
-                <TwitterIcon />
+          {teamMembers.map((member, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center p-5 border border-grey-shade-15 rounded-lg"
+            >
+              <div>
+                <Image
+                  src={member.imageSrc}
+                  width={317}
+                  height={253}
+                  alt={`${member.name} profile`}
+                />
               </div>
-              <div className="flex flex-col items-center justify-center gap-2">
-                <h2 className="font-semibold text-2xl max-desktop-lg:text-xl max-mobile-md:text-lg">
-                  Max Mitchell
-                </h2>
-                <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-base max-mobile-md:text-sm">
-                  Founder
-                </p>
+              <div className="flex flex-col gap-5 justify-center items-center mt-[-25px]">
+                <div className="border-none bg-purple-shade-60 py-3 px-5 rounded-full cursor-pointer">
+                  <TwitterIcon />
+                </div>
+                <div className="flex flex-col items-center justify-center gap-2">
+                  <h2 className="font-semibold text-2xl max-desktop-lg:text-xl max-mobile-md:text-lg">
+                    {member.name}
+                  </h2>
+                  <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-base max-mobile-md:text-sm">
+                    {member.role}
+                  </p>
+                </div>
               </div>
-            </div>
-            {/* input container */}
-            <div className="relative w-[85%] max-w-sm px-4 py-3 bg-[#E4E4E7] border border-grey-shade-15 dark:bg-grey-shade-8 max-mobile-md:w-full mt-5 rounded-full">
-              <input
-                type="text"
-                placeholder="Say Hello 👋"
-                className="w-full pr-10 bg-transparent dark:text-white dark:placeholder:text-gray-400 placeholder:text-lg max-desktop-lg:placeholder:text-base max-mobile-md:placeholder:text-sm focus:outline-none text-sm"
-              />
+              {/* input container */}
+              <div className="relative w-[85%] max-w-sm px-4 py-3 bg-[#E4E4E7] border border-grey-shade-15 dark:bg-grey-shade-8 max-mobile-md:w-full mt-5 rounded-full">
+                <input
+                  type="text"
+                  placeholder="Say Hello 👋"
+                  className="w-full pr-10 bg-transparent dark:text-white dark:placeholder:text-gray-400 placeholder:text-lg max-desktop-lg:placeholder:text-base max-mobile-md:placeholder:text-sm focus:outline-none text-sm"
+                />
 
-              <div className="absolute top-1/2 right-2 transform -translate-y-1/2 p-2 rounded-full bg-purple-shade-60 cursor-pointer">
-                <SendSmallIcon />
+                <div className="absolute top-1/2 right-2 transform -translate-y-1/2 p-2 rounded-full bg-purple-shade-60 cursor-pointer">
+                  <SendSmallIcon />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex flex-col items-center justify-center p-5 border border-grey-shade-15 rounded-lg">
-            <div>
-              <Image
-                src={`${
-                  process.env.NEXT_PUBLIC_BASE_PATH +
-                  "/images/about-us-team-one.png"
-                }`}
-                width={317}
-                height={253}
-                alt="profile icon"
-              />
-            </div>
-            <div className="flex flex-col gap-5 justify-center items-center mt-[-25px]">
-              <div className="border-none bg-purple-shade-60 py-3 px-5 rounded-full cursor-pointer">
-                <TwitterIcon />
-              </div>
-              <div className="flex flex-col items-center justify-center gap-2">
-                <h2 className="font-semibold text-2xl max-desktop-lg:text-xl max-mobile-md:text-lg">
-                  Max Mitchell
-                </h2>
-                <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-base max-mobile-md:text-sm">
-                  Founder
-                </p>
-              </div>
-            </div>
-            {/* input container */}
-            <div className="relative w-[85%] max-w-sm px-4 py-3 bg-[#E4E4E7] border border-grey-shade-15 dark:bg-grey-shade-8 max-mobile-md:w-full mt-5 rounded-full">
-              <input
-                type="text"
-                placeholder="Say Hello 👋"
-                className="w-full pr-10 bg-transparent dark:text-white dark:placeholder:text-gray-400 placeholder:text-lg max-desktop-lg:placeholder:text-base max-mobile-md:placeholder:text-sm focus:outline-none text-sm"
-              />
-
-              <div className="absolute top-1/2 right-2 transform -translate-y-1/2 p-2 rounded-full bg-purple-shade-60 cursor-pointer">
-                <SendSmallIcon />
-              </div>
-            </div>
-          </div>{" "}
-          <div className="flex flex-col items-center justify-center p-5 border border-grey-shade-15 rounded-lg">
-            <div>
-              <Image
-                src={`${
-                  process.env.NEXT_PUBLIC_BASE_PATH +
-                  "/images/about-us-team-one.png"
-                }`}
-                width={317}
-                height={253}
-                alt="profile icon"
-              />
-            </div>
-            <div className="flex flex-col gap-5 justify-center items-center mt-[-25px]">
-              <div className="border-none bg-purple-shade-60 py-3 px-5 rounded-full cursor-pointer">
-                <TwitterIcon />
-              </div>
-              <div className="flex flex-col items-center justify-center gap-2">
-                <h2 className="font-semibold text-2xl max-desktop-lg:text-xl max-mobile-md:text-lg">
-                  Max Mitchell
-                </h2>
-                <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-base max-mobile-md:text-sm">
-                  Founder
-                </p>
-              </div>
-            </div>
-            {/* input container */}
-            <div className="relative w-[85%] max-w-sm px-4 py-3 bg-[#E4E4E7] border border-grey-shade-15 dark:bg-grey-shade-8 max-mobile-md:w-full mt-5 rounded-full">
-              <input
-                type="text"
-                placeholder="Say Hello 👋"
-                className="w-full pr-10 bg-transparent dark:text-white dark:placeholder:text-gray-400 placeholder:text-lg max-desktop-lg:placeholder:text-base max-mobile-md:placeholder:text-sm focus:outline-none text-sm"
-              />
-
-              <div className="absolute top-1/2 right-2 transform -translate-y-1/2 p-2 rounded-full bg-purple-shade-60 cursor-pointer">
-                <SendSmallIcon />
-              </div>
-            </div>
-          </div>{" "}
-          <div className="flex flex-col items-center justify-center p-5 border border-grey-shade-15 rounded-lg">
-            <div>
-              <Image
-                src={`${
-                  process.env.NEXT_PUBLIC_BASE_PATH +
-                  "/images/about-us-team-one.png"
-                }`}
-                width={317}
-                height={253}
-                alt="profile icon"
-              />
-            </div>
-            <div className="flex flex-col gap-5 justify-center items-center mt-[-25px]">
-              <div className="border-none bg-purple-shade-60 py-3 px-5 rounded-full cursor-pointer">
-                <TwitterIcon />
-              </div>
-              <div className="flex flex-col items-center justify-center gap-2">
-                <h2 className="font-semibold text-2xl max-desktop-lg:text-xl max-mobile-md:text-lg">
-                  Max Mitchell
-                </h2>
-                <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-base max-mobile-md:text-sm">
-                  Founder
-                </p>
-              </div>
-            </div>
-            {/* input container */}
-            <div className="relative w-[85%] max-w-sm px-4 py-3 bg-[#E4E4E7] border border-grey-shade-15 dark:bg-grey-shade-8 max-mobile-md:w-full mt-5 rounded-full">
-              <input
-                type="text"
-                placeholder="Say Hello 👋"
-                className="w-full pr-10 bg-transparent dark:text-white dark:placeholder:text-gray-400 placeholder:text-lg max-desktop-lg:placeholder:text-base max-mobile-md:placeholder:text-sm focus:outline-none text-sm"
-              />
-
-              <div className="absolute top-1/2 right-2 transform -translate-y-1/2 p-2 rounded-full bg-purple-shade-60 cursor-pointer">
-                <SendSmallIcon />
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
