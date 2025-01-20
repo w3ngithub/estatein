c0a40f1cf3a96c19ea98da4396730e172036eb97
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-20 09:30:42 +0545

    our client section

--- src/components/modules/about-us/OurClients.tsx
+++ src/components/modules/about-us/OurClients.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { ClientThunderIcon, SudokuIcon } from "@/svgs/AboutUsSvg";
 import { ThreeStars } from "@/svgs/HomePageSvg";
 import React from "react";
 
@@ -20,7 +22,65 @@ const OurClients = () => {
           </p>
         </div>
         {/* card */}
-        {/* single card:- see our achievement section to make card */}
+        {/* single card:- see our achievement section to make single card */}
+        <div className="">
+          <div className="">
+            <div className="flex flex-col gap-10">
+              <div className="flex flex-row justify-between gap-2">
+                <div>
+                  <p
+                    className="text-grey-shade-60 font-medium text-lg
+                  "
+                  >
+                    Since 2019
+                  </p>
+                  <h1 className="font-semibold text-3xl">ABC Corporation</h1>
+                </div>
+                <Button>Visit Website</Button>
+              </div>
+              <div className="grid grid-cols-2 gap-3">
+                {/* first  */}
+                <div className="flex flex-col gap-3">
+                  <div className="flex flex-row gap-2 items-center">
+                    <div>
+                      <SudokuIcon />
+                    </div>
+                    <p className="text-grey-shade-60 font-medium text-lg">
+                      Domain
+                    </p>
+                  </div>
+                  <h1 className="font-medium text-xl">
+                    Commercial Real Estate
+                  </h1>
+                </div>
+                {/* second */}
+                <div className="flex flex-col gap-3">
+                  <div className="flex flex-row gap-2 items-center">
+                    <div>
+                      <ClientThunderIcon />
+                    </div>
+                    <p className="text-grey-shade-60 font-medium text-lg">
+                      Category
+                    </p>
+                  </div>
+                  <h1 className="font-medium text-xl">
+                    Luxury Home Development
+                  </h1>
+                </div>
+              </div>
+              <div className="flex flex-col gap-3 border border-grey-shade-15 p-5">
+                <p className="text-grey-shade-60 text-lg font-medium">
+                  What They Said 🤗
+                </p>
+                <p className="font-medium text-lg">
+                  Estatein's expertise in finding the perfect office space for
+                  our expanding operations was invaluable. They truly understand
+                  our business needs.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
