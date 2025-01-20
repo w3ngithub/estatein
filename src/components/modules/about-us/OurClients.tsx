982a24d37ca5c5e55075795d79acb165f5ada1fc
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-20 10:28:45 +0545

    carousel

--- src/components/modules/about-us/OurClients.tsx
+++ src/components/modules/about-us/OurClients.tsx
@@ -1,92 +1,153 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { ClientThunderIcon, SudokuIcon } from "@/svgs/AboutUsSvg";
 import { ThreeStars } from "@/svgs/HomePageSvg";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { carouselDataOurValuedClients } from "@/utilityComponents/aboutUsPage/ourValuedClients";
+import Link from "next/link";
 
 const OurClients = () => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap());
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+  }, [api]);
+
   return (
-    <section className="container py-10">
-      <div className="flex flex-col gap-16">
-        <div className="flex flex-col gap-3">
-          {/* 3 star logo */}
-          <div>
-            <ThreeStars />
+    <Carousel setApi={setApi}>
+      <section className="container flex flex-col gap-10 pt-5 max-mobile-md:gap-5">
+        <div className="flex flex-row justify-between items-end">
+          <div className="flex flex-col gap-3">
+            {/* 3 star logo */}
+            <div>
+              <ThreeStars />
+            </div>
+            <h1 className="dark:text-white text-5xl font-semibold max-desktop-lg:text-4xl max-tablet-sm:text-[28px]">
+              Frequently Asked Questions
+            </h1>
+            <p className="max-w-[900px] font-medium dark:text-grey-shade-60 text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
+              Find answers to common questions about Estatein&apos;s services,
+              property listings, and the real estate process. We&apos;re here to
+              provide clarity and assist you every step of the way.
+            </p>
           </div>
-          <h1 className="dark:text-white text-5xl font-semibold max-desktop-lg:text-4xl max-tablet-sm:text-[28px]">
-            Our Valued Clients
-          </h1>
-          <p className="max-w-[900px] font-medium dark:text-grey-shade-60 text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
-            At Estatein, we have had the privilege of working with a diverse
-            range of clients across various industries. Here are some of the
-            clients we've had the pleasure of serving
-          </p>
         </div>
-        {/* card */}
-        <div className="grid grid-cols-2">
-          {/* single card:- see our achievement section to make single card */}
-          <div className="p-2 dark:bg-grey-shade-d61 rounded-lg">
-            <div className="dark:bg-grey-shade-8 rounded-lg">
-              {/* container */}
-              <div className="flex flex-col gap-10 p-5">
-                <div className="flex flex-row justify-between items-center gap-2">
-                  <div className="flex flex-col gap-2">
-                    <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-base max-tablet-sm:text-sm">
-                      Since 2019
-                    </p>
-                    <h1 className="font-semibold text-3xl max-desktop-lg:text-2xl max-tablet-sm:text-xl">
-                      ABC Corporation
-                    </h1>
-                  </div>
-                  <Button className="py-6 px-4 border-2 border-grey-shade-15 bg-grey-shade-10 rounded-lg text-lg max-desktop-2xl:text-sm max-mobile-md:w-full hover:bg-grey-shade-15 dark:text-white">
-                    Visit Website
-                  </Button>
-                </div>
-                <div className="grid grid-cols-2 gap-3">
-                  {/* first  */}
-                  <div className="flex flex-col gap-3 border-r border-grey-shade-15">
-                    <div className="flex flex-row gap-2 items-center max-desktop-lg:text-sm">
-                      <div>
-                        <SudokuIcon />
+        {/* Carousel section */}
+        <div className="flex justify-center items-center my-2 h-full">
+          <CarouselContent>
+            {carouselDataOurValuedClients.map((item) => (
+              <CarouselItem className="mobile-xl:basis-1/2">
+                <div className="h-full flex-grow">
+                  <div className="flex flex-col gap-10 p-5">
+                    <div className="flex flex-row justify-between items-center gap-2">
+                      <div className="flex flex-col gap-2">
+                        <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-base max-tablet-sm:text-sm">
+                          Since {item.established}
+                        </p>
+                        <h1 className="font-semibold text-3xl max-desktop-lg:text-2xl max-tablet-sm:text-xl">
+                          {item.companyName}
+                        </h1>
                       </div>
-                      <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-sm max-tablet-sm:text-xs">
-                        Domain
+                      <Link href={`${item.companyUrl}`}>
+                        <Button className="py-6 px-4 border-2 border-grey-shade-15 bg-grey-shade-10 rounded-lg text-lg max-desktop-2xl:text-sm max-mobile-md:w-full hover:bg-grey-shade-15 dark:text-white">
+                          Visit Website
+                        </Button>
+                      </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-3">
+                      {/* first  */}
+                      <div className="flex flex-col gap-3 border-r border-grey-shade-15">
+                        <div className="flex flex-row gap-2 items-center max-desktop-lg:text-sm">
+                          <div>
+                            <SudokuIcon />
+                          </div>
+                          <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-sm max-tablet-sm:text-xs">
+                            Domain
+                          </p>
+                        </div>
+                        <h1 className="font-medium text-xl max-desktop-lg:text-base max-tablet-sm:text-sm">
+                          {item.domain}
+                        </h1>
+                      </div>
+                      {/* second */}
+                      <div className="flex flex-col gap-3">
+                        <div className="flex flex-row gap-2 items-center max-desktop-lg:text-sm">
+                          <div>
+                            <ClientThunderIcon />
+                          </div>
+                          <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-sm max-tablet-sm:text-xs">
+                            Category
+                          </p>
+                        </div>
+                        <h1 className="font-medium text-xl max-desktop-lg:text-base max-tablet-sm:text-sm">
+                          {item.category}
+                        </h1>
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-3 border border-grey-shade-15 p-5 rounded-lg">
+                      <p className="text-grey-shade-60 text-lg font-medium max-desktop-lg:text-base max-tablet-sm:text-sm">
+                        What They Said 🤗
+                      </p>
+                      <p className="font-medium text-lg max-desktop-lg:text-base max-tablet-sm:text-sm">
+                        {item.description}
                       </p>
                     </div>
-                    <h1 className="font-medium text-xl max-desktop-lg:text-base max-tablet-sm:text-sm">
-                      Commercial Real Estate
-                    </h1>
-                  </div>
-                  {/* second */}
-                  <div className="flex flex-col gap-3">
-                    <div className="flex flex-row gap-2 items-center max-desktop-lg:text-sm">
-                      <div>
-                        <ClientThunderIcon />
-                      </div>
-                      <p className="text-grey-shade-60 font-medium text-lg max-desktop-lg:text-sm max-tablet-sm:text-xs">
-                        Category
-                      </p>
-                    </div>
-                    <h1 className="font-medium text-xl max-desktop-lg:text-base max-tablet-sm:text-sm">
-                      Luxury Home Development
-                    </h1>
                   </div>
                 </div>
-                <div className="flex flex-col gap-3 border border-grey-shade-15 p-5 rounded-lg">
-                  <p className="text-grey-shade-60 text-lg font-medium max-desktop-lg:text-base max-tablet-sm:text-sm">
-                    What They Said 🤗
-                  </p>
-                  <p className="font-medium text-lg max-desktop-lg:text-base max-tablet-sm:text-sm">
-                    Estatein's expertise in finding the perfect office space for
-                    our expanding operations was invaluable. They truly
-                    understand our business needs.
-                  </p>
-                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </div>
+        <div className="flex flex-row justify-between mb-4 max-mobile-md:hidden border-t-[1px] border-t-[#E4E4E7] dark:border-t-grey-shade-15 dark:bg-grey-shade-8 pt-5">
+          <div className="text-base font-medium">
+            <span className="dark:text-white">
+              {String(current + 1).padStart(2, "0")}
+            </span>{" "}
+            <span className="dark:text-grey-shade-60">
+              of {String(count).padStart(2, "0")}
+            </span>
+          </div>
+          <div className="flex flex-row gap-3">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+        </div>
+        {/* for mobile screen */}
+        <div className="hidden max-mobile-md:block">
+          <div className="flex flex-row justify-between mb-3 border-t-[1px] border-t-[#E4E4E7] dark:border-t-grey-shade-15 dark:bg-grey-shade-8 pt-5">
+            <div className="flex flex-row items-center justify-center gap-3">
+              <CarouselPrevious />
+              <div className="text-base font-medium">
+                <span className="text-white">
+                  {String(current + 1).padStart(2, "0")}
+                </span>{" "}
+                <span className="text-grey-shade-60">
+                  of {String(count).padStart(2, "0")}
+                </span>
               </div>
+              <CarouselNext />
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Carousel>
   );
 };
 
---
+++ src/utilityComponents/aboutUsPage/ourValuedClients.tsx
@@ -0,0 +1,72 @@
+export const carouselDataOurValuedClients = [
+  {
+    id: 1,
+    established: "2019",
+    companyName: "ABC Corporation",
+    companyUrl: "/",
+    domain: "Commercial Real Estate",
+    category: "Luxury Home Development",
+    description:
+      "Estatein's expertise in finding the perfect office space for our expanding operations was invaluable. They truly understand our business needs.",
+  },
+  {
+    id: 2,
+    established: "2018",
+    companyName: "GreenTech Enterprises",
+    companyUrl: "/",
+    domain: "Commercial Real Estate",
+    category: "retail Space",
+    description:
+      "Estatein's ability to identify prime retail locations helped us expand our brand presence. They are a trusted partner in our growth.",
+  },
+  {
+    id: 3,
+    established: "2019",
+    companyName: "ABC Corporation",
+    companyUrl: "/",
+    domain: "Commercial Real Estate",
+    category: "Luxury Home Development",
+    description:
+      "Estatein's expertise in finding the perfect office space for our expanding operations was invaluable. They truly understand our business needs.",
+  },
+  {
+    id: 4,
+    established: "2019",
+    companyName: "ABC Corporation",
+    companyUrl: "/",
+    domain: "Commercial Real Estate",
+    category: "Luxury Home Development",
+    description:
+      "Estatein's expertise in finding the perfect office space for our expanding operations was invaluable. They truly understand our business needs.",
+  },
+  {
+    id: 5,
+    established: "2019",
+    companyName: "ABC Corporation",
+    companyUrl: "/",
+    domain: "Commercial Real Estate",
+    category: "Luxury Home Development",
+    description:
+      "Estatein's expertise in finding the perfect office space for our expanding operations was invaluable. They truly understand our business needs.",
+  },
+  {
+    id: 6,
+    established: "2019",
+    companyName: "ABC Corporation",
+    companyUrl: "/",
+    domain: "Commercial Real Estate",
+    category: "Luxury Home Development",
+    description:
+      "Estatein's expertise in finding the perfect office space for our expanding operations was invaluable. They truly understand our business needs.",
+  },
+  {
+    id: 7,
+    established: "2019",
+    companyName: "ABC Corporation",
+    companyUrl: "/",
+    domain: "Commercial Real Estate",
+    category: "Luxury Home Development",
+    description:
+      "Estatein's expertise in finding the perfect office space for our expanding operations was invaluable. They truly understand our business needs.",
+  },
+];
