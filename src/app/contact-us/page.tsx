5b63bee05d643c491f7813b619dae6a8e9163647
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-20 16:34:02 +0545

    discover location section

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { getInTouchCardData } from "@/utilityComponents/aboutUsPage/getInTouchCardData";
 import GetInTouchCard from "@/components/modules/contact-us/GetInTouchCard";
 import LetsConnectForm from "@/components/modules/contact-us/LetsConnectForm";
+import DiscoverLocation from "@/components/modules/contact-us/DiscoverLocation";
 
 const page = () => {
   return (
@@ -10,6 +11,7 @@ const page = () => {
       <GetInTouch />
       <GetInTouchCard data={getInTouchCardData} />
       <LetsConnectForm />
+      <DiscoverLocation />
     </div>
   );
 };
---
+++ src/components/modules/contact-us/DiscoverLocation.tsx
@@ -0,0 +1,31 @@
+import { ThreeStars } from "@/svgs/HomePageSvg";
+import React from "react";
+
+const DiscoverLocation = () => {
+  return (
+    <section className="container flex flex-col gap-10 py-10 max-mobile-md:gap-5 max-mobile-md:pb-8">
+      {/* first section */}
+      <div className="flex flex-row justify-between items-end">
+        <div className="flex flex-col gap-3">
+          {/* 3 star logo */}
+          <div>
+            <ThreeStars />
+          </div>
+          <h1 className="dark:text-white text-5xl font-semibold max-desktop-lg:text-4xl max-tablet-sm:text-[28px]">
+            Discover Our Office Locations
+          </h1>
+          <p className="max-w-[900px] dark:text-grey-shade-60 text-lg leading-6 max-desktop-lg:text-base max-tablet-sm:text-sm">
+            Estatein is here to serve you across multiple locations. Whether
+            you're looking to meet our team, discuss real estate opportunities,
+            or simply drop by for a chat, we have offices conveniently located
+            to serve your needs. Explore the categories below to find the
+            Estatein office nearest to you
+          </p>
+        </div>
+      </div>
+      {/* second section */}
+    </section>
+  );
+};
+
+export default DiscoverLocation;
