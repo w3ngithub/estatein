d655fc56b4a11563310149d7281756f7022b76e5
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-22 10:59:23 +0545

    login page

---
+++ src/app/(protected)/admin/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/modules/auth/LoginForm";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/modules/auth/LoginForm.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const LoginForm = () => {
+  const LoginSchema = z.object({
+    email: z.string().email({
+      message: "Email is required",
+    }),
+    password: z.string().min(1, {
+      message: "Password is required",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    // console.log(values);
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>User Name</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="John Doe" type="text" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="******" type="password" />
+                </FormControl>
+                <Button
+                  size="sm"
+                  variant="link"
+                  asChild
+                  className="px-0 font-normal"
+                >
+                  {/* <Link href="/auth/reset">Forgot password?</Link> */}
+                </Button>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default LoginForm;
