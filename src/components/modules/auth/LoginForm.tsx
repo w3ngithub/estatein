ba40bf3fa7a8d8679a38820fa25266a70b276bf8
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-22 11:52:03 +0545

    login page

--- src/components/modules/auth/LoginForm.tsx
+++ src/components/modules/auth/LoginForm.tsx
@@ -13,13 +13,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 const LoginForm = () => {
   const LoginSchema = z.object({
     email: z.string().email({
       message: "Email is required",
     }),
-    password: z.string().min(1, {
+    password: z.string().min(4, {
       message: "Password is required",
     }),
   });
@@ -37,46 +38,59 @@ const LoginForm = () => {
   };
 
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>User Name</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="John Doe" type="text" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="******" type="password" />
-                </FormControl>
-                <Button
-                  size="sm"
-                  variant="link"
-                  asChild
-                  className="px-0 font-normal"
-                >
-                  {/* <Link href="/auth/reset">Forgot password?</Link> */}
-                </Button>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
-    </>
+    <div className="flex flex-row justify-center items-center py-10">
+      <Card className="w-[400px] shadow-md">
+        <CardHeader>
+          <div className="flex items-center justify-center">
+            <h1 className="text-3xl font-semibold">Login</h1>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>User Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="John Doe" type="text" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="******" type="password" />
+                    </FormControl>
+                    <Button
+                      size="sm"
+                      variant="link"
+                      asChild
+                      className="px-0 font-normal"
+                    ></Button>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full border-2 bg-purple-shade-60 hover:bg-purple-shade-d60 text-white"
+              >
+                Login
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
