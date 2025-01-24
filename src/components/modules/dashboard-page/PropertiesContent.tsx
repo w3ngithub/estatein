b77246b193fe7cdb855ef09f0585ce73a3e779fb
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-24 09:24:49 +0545

    image upload added

--- src/components/modules/dashboard-page/PropertiesContent.tsx
+++ src/components/modules/dashboard-page/PropertiesContent.tsx
@@ -14,8 +14,13 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { ImagePlus } from "lucide-react";
 
 const PropertiesContent = () => {
+  const [preview, setPreview] = useState<string | ArrayBuffer | null>("");
+
   const formSchema = z.object({
     title: z.string().min(1, {
       message: "Title is required",
@@ -29,6 +34,10 @@ const PropertiesContent = () => {
     pillName: z.string().min(1, {
       message: "Pill Name is required",
     }),
+    image: z
+      //Rest of validations done via react dropzone
+      .instanceof(File)
+      .refine((file) => file.size !== 0, "Please upload an image"),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -38,11 +47,36 @@ const PropertiesContent = () => {
       description: "",
       price: "",
       pillName: "",
+      image: new File([""], "filename"),
     },
   });
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const reader = new FileReader();
+      try {
+        reader.onload = () => setPreview(reader.result);
+        reader.readAsDataURL(acceptedFiles[0]);
+        form.setValue("image", acceptedFiles[0]);
+        form.clearErrors("image");
+      } catch (error) {
+        setPreview(null);
+        form.resetField("image");
+      }
+    },
+    [form]
+  );
+
+  const { getRootProps, getInputProps, isDragActive, fileRejections } =
+    useDropzone({
+      onDrop,
+      maxFiles: 1,
+      maxSize: 1000000,
+      accept: { "image/png": [], "image/jpg": [], "image/jpeg": [] },
+    });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
+    console.log(`Image uploaded successfully 🎉 ${values.image.name}`);
   }
 
   return (
@@ -118,6 +152,63 @@ const PropertiesContent = () => {
                     />
                   </FormControl>
                   <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* image upload */}
+            <FormField
+              control={form.control}
+              name="image"
+              render={() => (
+                <FormItem className="mx-auto md:w-1/2">
+                  <FormLabel
+                    className={`${
+                      fileRejections.length !== 0 && "text-destructive"
+                    }`}
+                  >
+                    <h2 className="text-xl font-semibold tracking-tight">
+                      Upload your image
+                      <span
+                        className={
+                          form.formState.errors.image ||
+                          fileRejections.length !== 0
+                            ? "text-destructive"
+                            : "text-muted-foreground"
+                        }
+                      ></span>
+                    </h2>
+                  </FormLabel>
+                  <FormControl>
+                    <div
+                      {...getRootProps()}
+                      className="mx-auto flex cursor-pointer flex-col items-center justify-center gap-y-2 rounded-lg border border-foreground p-8 shadow-sm shadow-foreground"
+                    >
+                      {preview && (
+                        <img
+                          src={preview as string}
+                          alt="Uploaded image"
+                          className="max-h-[400px] rounded-lg"
+                        />
+                      )}
+                      <ImagePlus
+                        className={`size-40 ${preview ? "hidden" : "block"}`}
+                      />
+                      <Input {...getInputProps()} type="file" />
+                      {isDragActive ? (
+                        <p>Drop the image!</p>
+                      ) : (
+                        <p>Click here or drag an image to upload it</p>
+                      )}
+                    </div>
+                  </FormControl>
+                  <FormMessage>
+                    {fileRejections.length !== 0 && (
+                      <p>
+                        Image must be less than 1MB and of type png, jpg, or
+                        jpeg
+                      </p>
+                    )}
+                  </FormMessage>
                 </FormItem>
               )}
             />
