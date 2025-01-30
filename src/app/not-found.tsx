a1971316b572c296f2b8821cab4e76ae78dbb9be
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-30 14:10:26 +0545

    page not found added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col gap-6 justify-center items-center h-screen bg-gradient-to-br from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800 text-center px-4">
+    <div className="flex flex-col gap-6 justify-center items-center h-[85vh] bg-gradient-to-br from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800 text-center px-4">
       <h1 className="text-7xl font-extrabold text-purple-700 dark:text-purple-400 max-desktop-2xl:text-6xl max-mobile-lg:text-5xl">
         Whooops!
       </h1>
