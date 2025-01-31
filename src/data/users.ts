0dd316acce4cbbdd465b6c93adcf9366893106e1
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-01-31 14:17:35 +0545

    typescript added

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,24 +1,17 @@
-interface User {
-  userName: string;
-  password: string;
+enum Users {
+  ADMIN = "admin",
+  USER1 = "dipesh",
+  USER2 = "mukesh",
 }
 
-const users: User[] = [
-  {
-    userName: "admin",
-    password: "admin",
-  },
-  {
-    userName: "dipesh",
-    password: "dipesh",
-  },
-  {
-    userName: "mukesh",
-    password: "mukesh",
-  },
-];
+const userCredentials: Record<Users, { userName: Users; password: string }> = {
+  [Users.ADMIN]: { userName: Users.ADMIN, password: "admin" },
+  [Users.USER1]: { userName: Users.USER1, password: "dipesh" },
+  [Users.USER2]: { userName: Users.USER2, password: "mukesh" },
+};
 
-export const getUserByUserName = (userName: string): User | undefined => {
-  const found = users.find((user) => user.userName === userName);
-  return found;
+export const getUserByUserName = (
+  userName: string
+): { userName: Users; password: string } | undefined => {
+  return userCredentials[userName as Users];
 };
