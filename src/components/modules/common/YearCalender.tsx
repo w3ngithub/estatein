846aa8cc35ff35b89395850982499d5413841f73
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-02-03 12:17:24 +0545

    build error fixed

--- src/components/modules/common/YearCalender.tsx
+++ src/components/modules/common/YearCalender.tsx
@@ -8,13 +8,12 @@ import { z } from "zod";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { FormControl, FormField, FormItem } from "@/components/ui/form";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { CalenderIcon } from "@/svgs/PropertyPageSvg";
 import { useEffect, useState } from "react";
 
 interface YearCalendarProps {
