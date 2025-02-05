44ab3d9e3063aaafd71c17ebcf441c338e3fac22
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-02-05 14:55:16 +0545

    loader added

--- src/components/modules/dashboard-page/AddPropertySizeModal.tsx
+++ src/components/modules/dashboard-page/AddPropertySizeModal.tsx
@@ -22,6 +22,7 @@ import {
 import { nanoid } from "nanoid";
 import propertySize from "@/utilityComponents/dashboardPage/propertySizeTypeData.json";
 import { toast } from "sonner";
+import Loading from "@/components/elements/Loading";
 
 interface AddPropertySizeTypeModalProps {
   isModalOpenPropertySize: boolean;
@@ -35,6 +36,7 @@ const AddPropertySizeTypeModal = ({
   const [propertyTypeSizeData, setPropertyTypeSizeData] = useState([
     ...propertySize,
   ]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const AddPropertyTypeSchema = z.object({
     propertySizeType: z.string().min(1, {
@@ -50,6 +52,7 @@ const AddPropertySizeTypeModal = ({
   });
 
   const onSubmit = async (values: z.infer<typeof AddPropertyTypeSchema>) => {
+    setIsLoading(true);
     const newPropertySize = {
       id: nanoid(),
       value: values.propertySizeType,
@@ -77,10 +80,10 @@ const AddPropertySizeTypeModal = ({
         toast.success("Property Size Type is added");
       }
     } catch (error) {
-      console.error("Error creating property size type:", error);
       toast.error("Error creating property size type");
     }
 
+    setIsLoading(false);
     setIsModalOpenPropertySize(false);
     form.reset();
   };
@@ -91,54 +94,58 @@ const AddPropertySizeTypeModal = ({
       onOpenChange={setIsModalOpenPropertySize}
     >
       <DialogContent className="rounded-lg">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
-            <DialogHeader>
-              <DialogTitle>Add Property Size Type</DialogTitle>
-              <DialogDescription> </DialogDescription>
-            </DialogHeader>
-            <div className="space-y-4">
-              <FormField
-                control={form.control}
-                name="propertySizeType"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row gap-5 items-center">
-                    {/* <FormLabel></FormLabel> */}
-                    <div className="flex flex-col gap-3 w-full">
-                      <FormControl>
-                        <Input
-                          {...field}
-                          placeholder="Enter Property Size Type"
-                          type="text"
-                          className="h-14"
-                        />
-                      </FormControl>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+              <DialogHeader>
+                <DialogTitle>Add Property Size Type</DialogTitle>
+                <DialogDescription> </DialogDescription>
+              </DialogHeader>
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="propertySizeType"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row gap-5 items-center">
+                      {/* <FormLabel></FormLabel> */}
+                      <div className="flex flex-col gap-3 w-full">
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder="Enter Property Size Type"
+                            type="text"
+                            className="h-14"
+                          />
+                        </FormControl>
 
-                      <FormMessage />
-                    </div>
-                  </FormItem>
-                )}
-              />
-            </div>
-            <DialogFooter>
-              <div className="flex flex-row justify-between items-center">
-                <Button
-                  onClick={() => setIsModalOpenPropertySize(false)}
-                  variant="destructive"
-                  className="px-6"
-                >
-                  Cancel
-                </Button>
-                <Button
-                  type="submit"
-                  className="bg-purple-shade-60 hover:bg-purple-shade-d60 text-white px-7"
-                >
-                  Save
-                </Button>
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
               </div>
-            </DialogFooter>
-          </form>
-        </Form>
+              <DialogFooter>
+                <div className="flex flex-row justify-between items-center">
+                  <Button
+                    onClick={() => setIsModalOpenPropertySize(false)}
+                    variant="destructive"
+                    className="px-6"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    type="submit"
+                    className="bg-purple-shade-60 hover:bg-purple-shade-d60 text-white px-7"
+                  >
+                    Save
+                  </Button>
+                </div>
+              </DialogFooter>
+            </form>
+          </Form>
+        )}
       </DialogContent>
     </Dialog>
   );
