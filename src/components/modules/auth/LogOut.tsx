9cf95c8177dbdc6840f1c5eb32f82ecc65b95bc4
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2025-02-06 12:36:07 +0545

    switch user

--- src/components/modules/auth/LogOut.tsx
+++ src/components/modules/auth/LogOut.tsx
@@ -3,6 +3,9 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import React from "react";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { redirect } from "next/navigation";
 
 const LogOutComp = ({
   userName,
@@ -19,29 +22,48 @@ const LogOutComp = ({
     }
   };
   return (
-    <div className="flex flex-row gap-1 w-full">
-      {!collapsed && (
-        <Avatar>
-          <AvatarFallback>{userName?.charAt(0).toUpperCase()}</AvatarFallback>
-        </Avatar>
-      )}
-      {!collapsed && (
-        <Button
-          onClick={handleLogout}
-          variant="ghost"
-          className="w-full text-sm justify-start gap-2 dark:text-white"
-        >
-          Logout
-          <LogOut className="h-4 w-4" />
-        </Button>
-      )}
-      {collapsed && (
-        <div className="flex justify-center items-center w-full">
-          <Button onClick={handleLogout} variant="ghost">
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-row items-center">
+        {!collapsed && (
+          <Label className="text-sm" htmlFor="user">
+            Switch to User
+          </Label>
+        )}
+
+        <Switch
+          id="user"
+          onCheckedChange={(checked) => {
+            if (checked) {
+              redirect("/");
+            }
+          }}
+        />
+      </div>
+
+      <div className="flex flex-row gap-1 w-full">
+        {!collapsed && (
+          <Avatar>
+            <AvatarFallback>{userName?.charAt(0).toUpperCase()}</AvatarFallback>
+          </Avatar>
+        )}
+        {!collapsed && (
+          <Button
+            onClick={handleLogout}
+            variant="ghost"
+            className="w-full text-sm justify-start gap-2 dark:text-white"
+          >
+            Logout
             <LogOut className="h-4 w-4" />
           </Button>
-        </div>
-      )}
+        )}
+        {collapsed && (
+          <div className="flex justify-center items-center w-full">
+            <Button onClick={handleLogout} variant="ghost">
+              <LogOut className="h-4 w-4" />
+            </Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
